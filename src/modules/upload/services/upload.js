17495f1c732ce7246a3d9295b91004c870320bc3
syeda Faiza <syedafaiza2610@gmail.com>
2024-05-10 23:11:05 +0500

    Upload work is done

--- src/modules/upload/services/upload.js
+++ src/modules/upload/services/upload.js
@@ -3,6 +3,7 @@
 import { v2 as cloudinary } from 'cloudinary';
 import ENV from "../../../constants/index.js"
 import fsExtra from 'fs-extra';
+import sharp from 'sharp';
 
 
 cloudinary.config({
@@ -14,30 +15,30 @@ cloudinary.config({
 
 const uploadFile = async (file) => {
     return new Promise((resolve, reject) => {
-        const filepath = `src/modules/upload/files/${file.filename}`;
-        sharp(filepath)
-        .jpeg({quality : 40 })
-        .toFile(`resize-${filepath}`, async(err, info)=> {
-
-        })
-                cloudinary.uploader.upload(filepath,
-                    { 
-                        folder:"store_images",
-                        public_id:file.filename
+        const resizePath = `src/modules/upload/files/resize-${file.filename}`
+        const filePath = `src/modules/upload/files/${file.filename}`;
+        sharp(filePath)
+            .png({ quality: 10 })
+            .toFile(resizePath, async (err, info) => {
+                console.log("err===>" , err)
+                console.log("info==>" , info)
+                cloudinary.uploader.upload(resizePath,
+                    {
+                        folder: "store-images",
+                        public_id: file.filename
                     },
-                    function (error , result) { 
-                        if(error){
+                    function (error, result) {
+                        if (error) {
                             reject(error)
                             return;
                         }
-                        fsExtra.removeSync(filepath)
-                            resolve(result)  
+                        // fsExtra.removeSync(filePath)
+                        // fsExtra.removeSync(resizePath)
+                        // resolve(result)
                     });
-    });
+            })
 
-
-
-
+    })
 }
 
 export default uploadFile;
