04c8297bba2927b91255802a98c7e1ca105668f3
syeda Faiza <syedafaiza2610@gmail.com>
2024-05-31 23:12:34 +0500

    fixing port

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.get("/" ,(req ,res) =>{
 })
 
 app.use("/api" , routes ) //api pe req ayegi wo humy routes pe le jaega
+const port = ENV.port || 3000;
  
-app.listen(4000, () =>{
-    console.log("Server is running at port 4000")
+app.listen(port, () =>{
+    console.log(`Server is running at port ${port}`)
 })
